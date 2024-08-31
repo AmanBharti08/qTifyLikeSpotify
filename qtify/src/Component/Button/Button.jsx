43a7078acdf2aef7ad59fc8827a686dec2b3b1c0
AmanBharti08 <amanbharti0897@gmail.com>
2024-08-31 19:03:27 +0530

    first change

---
+++ qtify/src/Component/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Button.module.css";
+const Button = ({ text }) => {
+  return (
+    <div className={styles.container}>
+      <button className={styles.button}>{text}</button>
+    </div>
+  );
+};
+
+export default Button;
