93311b34ec5a4bf8d46d33fb161ab38be9c441cb
AmanBharti08 <amanbharti0897@gmail.com>
2024-08-31 19:54:10 +0530

    second change

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,13 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import Hero from "./Component/Hero/Hero";
 import Navbar from "./Component/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
